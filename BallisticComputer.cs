2ab76c96eb9c01094616b8afd1763086b53947f8
ArchiMikael <archimikael@gmail.com>
2024-04-18 17:28:44 +0300

    Initial commit (source files for Unity)

---
+++ BallisticComputer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallisticComputer : MonoBehaviour
+{
+    public float HorzOffset = 0.0f;
+    public float VertOffset = 0.0f;
+    public float InitialSpeed = 0.0f;
+    public float Drag = 0.0f;
+
+    public int Iterations = 10;
+    public bool Compute = false;
+    public bool SlowPrediction = false;
+
+    public float PredictedAngle = 0.0f;
+
+    private float Epsilon = 1.0E-8f;
+    private float Grav = 9.8f;
+    private GameObject Proj;
+    private BallisticMovementHandler ProjMovHndlr;
+
+    void Update()
+    {
+        if(Compute) {
+            PredictedAngle = (1.0f / 2.0f * Mathf.Asin(Grav * HorzOffset / Mathf.Pow(InitialSpeed, 2)));
+            for(int i = 0; i < Iterations; i++) {
+                PredictedAngle = PredictedAngle - ((SlowPrediction) ? (Epsilon) : (1)) *
+                                                  ((Drag * HorzOffset * Mathf.Tan(PredictedAngle)) + ((Grav * HorzOffset) / (InitialSpeed * Mathf.Cos(PredictedAngle))) + (Grav / Drag * Mathf.Log(1.0f - (Drag * HorzOffset) / (InitialSpeed * Mathf.Cos(PredictedAngle)))) - (Drag * VertOffset)) /
+                                                  (((Drag * HorzOffset) / (Mathf.Pow(Mathf.Cos(PredictedAngle), 2))) + 
+                                                  ((Grav * HorzOffset * Mathf.Sin(PredictedAngle)) / (InitialSpeed * Mathf.Pow(Mathf.Cos(PredictedAngle), 2))) +
+                                                  (Grav / Drag * (1.0f / (1.0f - (Drag * HorzOffset) / (InitialSpeed * Mathf.Cos(PredictedAngle)))) * (Drag * HorzOffset * Mathf.Sin(PredictedAngle)) / (InitialSpeed * Mathf.Pow(Mathf.Cos(PredictedAngle), 2))));
+            }
+            Proj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Proj.transform.Translate(0.0f, 1.0f, 0.0f);
+            Proj.transform.localScale = new Vector3(15.0f, 15.0f, 50.0f);
+            ProjMovHndlr = Proj.AddComponent<BallisticMovementHandler>();
+            ProjMovHndlr.Drag = Drag;
+            ProjMovHndlr.InitialSpeed = InitialSpeed;
+            ProjMovHndlr.InitialVertAngle = PredictedAngle;
+            ProjMovHndlr.MarkTrajectory = true;
+            Compute = false;
+        }
+    }
+}
---
+++ BallisticMovementHandler.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallisticMovementHandler : MonoBehaviour
+{
+    public float Drag = 0.0f;
+    public float InitialSpeed = 0.0f;
+    public float InitialVertAngle = 0.0f;
+    public float InitialHorzAngle = 0.0f;
+
+    public bool MarkTrajectory = false;
+    public bool StopOnZeroY = true;
+
+    private float Grav = 9.8f;
+
+    public Vector3 Speed = new Vector3(0.0f, 0.0f, 0.0f);
+    public Vector3 Acceleration = new Vector3(0.0f, 0.0f, 0.0f);
+    public Vector3 Direction = new Vector3(0.0f, 0.0f, 0.0f);
+
+    private int cntr = 49;
+    private GameObject newObj;
+
+    void Start() {
+        Direction.x = Mathf.Cos(InitialHorzAngle) * Mathf.Cos(InitialVertAngle);
+        Direction.y = Mathf.Sin(InitialVertAngle);
+        Direction.z = Mathf.Sin(InitialHorzAngle) * Mathf.Cos(InitialVertAngle);
+
+        Speed.x = Direction.x * InitialSpeed;
+        Speed.y = Direction.y * InitialSpeed;
+        Speed.z = Direction.z * InitialSpeed;
+    }
+
+    void FixedUpdate() {
+        Acceleration.x = -Drag * Speed.x;
+        Acceleration.y =  -Drag * Speed.y - Grav;
+        Acceleration.z = -Drag * Speed.z;
+        Speed += Acceleration * Time.fixedDeltaTime;
+
+        if(gameObject.transform.position.y <= 0.0f && StopOnZeroY) {
+            Acceleration.x = 0.0f;
+            Acceleration.y = 0.0f;
+            Acceleration.z = 0.0f;
+            Speed.x = 0.0f;
+            Speed.y = 0.0f;
+            Speed.z = 0.0f;
+        }
+
+        gameObject.transform.Translate(Speed.x * Time.fixedDeltaTime, Speed.y * Time.fixedDeltaTime, Speed.z * Time.fixedDeltaTime, Space.World);
+        gameObject.transform.LookAt(gameObject.transform.position + Speed);
+
+        if(MarkTrajectory) {
+            cntr++;
+            if(cntr == 50 && gameObject.transform.position.y > 0.0f) {
+                cntr = 0;
+                newObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                newObj.transform.Translate(gameObject.transform.position);
+                newObj.transform.localScale = new Vector3(5.0f, 5.0f, 5.0f);
+            }
+        }
+    }
+}
